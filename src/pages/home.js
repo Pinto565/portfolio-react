381039b9d7fd0de30a9d68fbc339bfc7612ee27d
Pinto565 <76471515+Pinto565@users.noreply.github.com>
2021-05-27 11:27:26 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,11 +4,15 @@ import Fade from "react-reveal/Fade";
 function Home() {
   return (
     <div>
-      <Fade bottom>
-        <div className="container">
-          <div className="home-content">
+      <div className="container">
+        <div className="home-content">
+          <Fade top>
             <h1>Pinto Infant</h1>
+          </Fade>
+          <Fade>
             <p>Front-end web developer</p>
+          </Fade>
+          <Fade bottom>
             <div className="social-icons">
               <a href="https://github.com/Pinto565">
                 <i className="fab fa-github"></i>
@@ -20,9 +24,9 @@ function Home() {
                 <i className="far fa-envelope"></i>
               </a>
             </div>
-          </div>
+          </Fade>
         </div>
-      </Fade>
+      </div>
     </div>
   );
 }
