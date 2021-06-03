2a439570efd87aaa5e44cf89f092f83d13762af3
Pinto565 <76471515+Pinto565@users.noreply.github.com>
2021-06-03 20:46:05 +0530

    Added Favicon

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,6 +8,7 @@ function Home() {
         <div className="container">
           <div className="home-content">
             <h1>Pinto Infant</h1>
+            <p>Web Developer</p>
             <div className="social-icons">
               <a href="https://github.com/Pinto565" target="_blank">
                 <i className="fab fa-github"></i>
