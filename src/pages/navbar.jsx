f43886dbaab15579e0b9a066ef65bd5ea4764100
Pinto565 <pintoinfant5650@gmail.com>
2021-08-30 09:27:51 +0000

    Fonts

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,23 +1,26 @@
 import { Link } from "react-router-dom";
 import "../App.css";
+import Fade from "react-reveal/Fade";
 
 export default function NavBar() {
   return (
     <div>
-      <div className="menu">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </div>
+      <Fade>
+        <div className="menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </div>
+      </Fade>
     </div>
   );
 }
