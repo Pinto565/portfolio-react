3742619924a8fae13d18dc52499d99974ceeda08
Pinto565 <pintoinfant5650@gmail.com>
2021-11-09 17:00:48 +0530

    content rearranged

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,16 +9,32 @@ function About() {
           <p className="about-title">tech i use</p>
           <div className="content-box">
             <div className="content">
+              <img src="https://img.icons8.com/ios-filled/100/ffffff/html-5--v1.png" />
+              <p>HTML</p>
+            </div>
+            <div className="content">
+              <img src="https://img.icons8.com/ios-filled/100/ffffff/javascript.png" />
+              <p>JAVASCRIPT</p>
+            </div>
+            <div className="content">
+              <img src="https://img.icons8.com/ios-glyphs/100/ffffff/react.png" />
+              <p>REACT JS</p>
+            </div>
+            <div className="content">
+              <img src="https://img.icons8.com/windows/100/ffffff/nodejs.png" />
+              <p>NODE JS</p>
+            </div>
+            <div className="content">
+              <img src="https://img.icons8.com/ios/100/ffffff/database.png" />
+              <p>SQL / NoSQL</p>
+            </div>
+            <div className="content">
               <img src="https://img.icons8.com/ios-filled/100/ffffff/python.png" />
               <p>PYTHON</p>
             </div>
             <div className="content">
               <img src="https://img.icons8.com/ios-filled/100/ffffff/linux.png" />
               <p>LINUX</p>
-            </div>
-            <div className="content">
-              <img src="https://img.icons8.com/ios-filled/100/ffffff/mysql-logo.png" />
-              <p>MYSQL</p>
             </div>
             <div className="content">
               <img src="https://img.icons8.com/ios-filled/100/ffffff/docker.png" />
@@ -28,22 +44,7 @@ function About() {
               <img src="https://img.icons8.com/ios/100/ffffff/kubernetes.png" />
               <p>MICROK8S</p>
             </div>
-            <div className="content">
-              <img src="https://img.icons8.com/ios-filled/100/ffffff/html-5--v1.png" />
-              <p>HTML</p>
-            </div>
-            <div className="content">
-              <img src="https://img.icons8.com/ios-glyphs/100/ffffff/css3.png" />
-              <p>CSS</p>
-            </div>
-            <div className="content">
-              <img src="https://img.icons8.com/ios-filled/100/ffffff/javascript.png" />
-              <p>JAVASCRIPT</p>
-            </div>
-            <div className="content">
-              <img src="https://img.icons8.com/windows/100/ffffff/nodejs.png" />
-              <p>NODE JS</p>
-            </div>
+
             <div className="content">
               <img src="https://img.icons8.com/ios-filled/100/ffffff/git.png" />
               <p>GIT</p>
