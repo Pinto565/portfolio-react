db4859389e23f5413d3bf2b24ce02486500391ce
Pinto565 <pintoinfant5650@gmail.com>
2022-06-16 19:40:27 +0530

    name error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./pages/styles/App.css"
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Projects from "./pages/projects";
+import Contact from "./pages/contact";
 import { Route } from "react-router-dom";
 
 function App() {
